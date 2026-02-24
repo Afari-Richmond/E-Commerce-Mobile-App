0d67e8538ccf7df8116e120cc2df3e4f9065a2b7
richmondafari <afaririchmond1000@gmail.com>
2026-02-24 11:19:58 +0000

    (fix): fix the tailwind config not showing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
 import { Stack } from "expo-router";
-import "./global.css"
+import "./global.css"; // Looks in the same folder (app/)
 
 export default function RootLayout() {
   return <Stack />;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // Add the paths to all of your component files so Tailwind can find your classes
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}", "./App.tsx"],
   presets: [require("nativewind/preset")], 
   theme: {
     extend: {},
